025a72e806ab5fd791d7f4fc3e8b1a49b68c3523
JasonGitHubLiu <jason.liu002@umb.edu>
2023-02-01 20:25:13 -0500

    Added Functions

--- script.js
+++ script.js
@@ -12,7 +12,45 @@ const winConditions = [
     [2, 4, 6]
 ]
 
-let options = ['', '', '' ,'', '', '' ,'', '', '' ]
+let options = ["", "", "" ,"", "", "" ,"", "", "" ]
 let currentPlayer = 'X';
 let running = false; 
 
+
+startGame();
+
+function startGame(){
+    cells.forEach(cell => cell.addEventListener('click', cellClicked))
+    restartButton.addEventListener('click', restartGame);
+    statusText.textContent = `It is ${currentPlayer}'s turn`
+    running = true;  // we set running to true to start the game since we set running = false earlier, on line 17
+}
+
+function cellClicked(){
+    const cellIndex = this.getAttribute('cellIndex')
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    changePlayer();
+    checkWinner();
+}
+
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer //Displays who's turn it is
+}
+
+function changePlayer(){ //this function changes players
+    currentPlayer = (currentPlayer == "X") ? "O" : "X" // take current player, set it equal to X. If current player is equal to X, reassign current player with O, otherwise, X
+    statusText.textContent = `It is ${currentPlayer}'s turn`
+}
+
+function checkWinner(){
+    
+}
+
+function restartGame(){
+    
+}
